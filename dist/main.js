7da94074d17e92f8531d4192548583ee9c4b1e10
Douglas Americo <douglas.alex.1212@gmail.com>
2026-07-22 22:52:59 -0300

    iniciando o projeto

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const numero = 12;
+const nome = prompt("Informe o nome ");
+console.log("O " + nome + " gosta do numero " + numero);
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+const numero : number = 12;
+const nome : string|null = prompt("Informe o nome ");
+console.log("O "+nome+" gosta do numero "+numero);
