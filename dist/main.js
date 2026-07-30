3a0e344f9ae055745eb4043650fd9df586fd8bb2
Douglas Americo <douglas.alex.1212@gmail.com>
2026-07-29 22:50:05 -0300

    aula 2

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,18 @@
 "use strict";
-const numero = 12;
-const nome = prompt("Informe o nome ");
-console.log("O " + nome + " gosta do numero " + numero);
+const n1 = prompt("Informe o numero: ");
+const n2 = prompt("Informe o numero: ");
+let n3 = 0, n4 = 0;
+if (n1 != "" && n2 != "") {
+    n3 = Number(n1);
+    n4 = Number(n2);
+    if (!isNaN(n3) && !isNaN(n4)) {
+        console.log("n1 -> " + n3);
+        console.log("n2 -> " + n4);
+    }
+    else {
+        console.log("Não eram numeros");
+    }
+}
+else {
+    console.log("Somente valores numericos");
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-const numero : number = 12;
-const nome : string|null = prompt("Informe o nome ");
-console.log("O "+nome+" gosta do numero "+numero);
+const n1 : string|null = prompt("Informe o numero: ");
+const n2 : string|null = prompt("Informe o numero: ");
+let n3 : number =0 , n4: number=0 ;
+
+if(n1 != "" && n2 != ""){
+    n3=Number(n1);
+    n4=Number(n2);
+    if(!isNaN(n3) && !isNaN(n4)){
+        console.log("n1 -> "+n3);
+        console.log("n2 -> "+n4);
+    }else{
+        console.log("Não eram numeros");
+    }
+}else{
+    console.log("Somente valores numericos");
+}
+
